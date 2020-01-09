901c5843f361ce7e6dc0cad4ce5d3afb54cb756e
Víctor Alonso González <vicks630@gmail.com>
2020-01-10 00:58:57 +0100

    Versión of DB for phase 2

--- adoptACat.sql
+++ adoptACat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 02-01-2020 a las 10:08:55
+-- Tiempo de generación: 10-01-2020 a las 00:57:14
 -- Versión del servidor: 5.7.28-0ubuntu0.18.04.4
 -- Versión de PHP: 7.2.24-0ubuntu0.18.04.1
 
@@ -30,7 +30,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `user_name` varchar(16) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `token` varchar(128) NOT NULL
+  `token` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -38,7 +38,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `user_name`, `password`, `token`) VALUES
-(1, 'victor', 'a1f8bbd098b1ac15b357ea2db8a528f7a266109a03b5fb385685eee7fa45a21e', 'c2abb3273e75648c9eae1e4687b03461f1161229aec533c5e2248ccf7eebbdcf5787af9bbde9cf46e682e5a99867267b73eaf9ba0a67ed118a075f0969f92997');
+(1, 'victor', 'a1f8bbd098b1ac15b357ea2db8a528f7a266109a03b5fb385685eee7fa45a21e', '944b03d757a7f953d03bee1c3c4d1fa00d609fc0d0bb45a6cd0f3cdc1749635608c6774427226e4dded23b78d352940e9786e8554b06c0dd2783d831b5a5c242'),
+(9, 'victor2', 'a1f8bbd098b1ac15b357ea2db8a528f7a266109a03b5fb385685eee7fa45a21e', '271b41f75bf864acba93c9b29b7ed919298d6829b8cf776822a11b5c418f1a4aa5bb291fd91739b8ea98db3ccbd57dc0f0acca15dc7014125342acdc384ac77f');
 
 --
 -- Índices para tablas volcadas
@@ -58,7 +59,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
